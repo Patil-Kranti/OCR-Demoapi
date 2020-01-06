3ecfa1f002f7a795dc61e422a2024b0d2cdcfb49
Kranti Patil <patilkranti03@gmail.com>
2020-01-07 01:05:59 +0530

    reset

--- api.py
+++ api.py
@@ -16,29 +16,24 @@ def home():
     try:
         if request.method == 'POST':
             imagedata = request.json.get("image").encode()
-            print("1")
 
             with open("img.png", "wb") as fh:
                 fh.write(base64.decodebytes(imagedata))
             image = cv2.imread("img.png")
             gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            print("2")
 
             cv2.imshow("Image", gray)
-            print("3")
 
             gray = cv2.threshold(gray, 0, 255,
                                  cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
-            print("4")
 
             filename = "{}.png".format(os.getpid())
             cv2.imwrite(filename, gray)
-            print("5")
 
             text = pytesseract.image_to_string(Image.open(filename))
             os.remove(filename)
             print(text)
-            print("6")
+           
 
             return jsonify(text)
 
